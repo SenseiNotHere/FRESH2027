8044c78b0046711ac7e201f5689758767224945e
SenseiNotHere <painsjonas@gmail.com>
2026-04-15 11:54:54 -0400

    Daaad PyLance was being annoying ahain

--- superstructure/superstructure_helpers.py
+++ superstructure/superstructure_helpers.py
@@ -7,7 +7,7 @@ from .robot_state import RobotState
 
 
 class SuperstructureHelpers:
-    def _handle_music_cleanup(self: "Superstructure"):
+    def _handle_music_cleanup(self: "Superstructure"): # type: ignore
         if not self.hasOrchestra:
             return
         if self.robot_state != RobotState.PLAYING_SONG and self.hasOrchestra:
--- superstructure/superstructure_states.py
+++ superstructure/superstructure_states.py
@@ -6,14 +6,14 @@ if TYPE_CHECKING:
 
 class SuperstructureStates:
     # Handles idle state by ensuring all subsystems are stopped.
-    def _handle_idle(self: "Superstructure"):
+    def _handle_idle(self: "Superstructure"): # type: ignore
         """
         Handles the idle state by stopping all subsystems.
         """
         pass
 
     # Start song on entry
-    def _handle_playing_song(self: "Superstructure"):
+    def _handle_playing_song(self: "Superstructure"): # type: ignore
         """
         Handles the playing song state by playing the current loaded song.
         """
@@ -21,7 +21,7 @@ class SuperstructureStates:
             self.orchestra.play_selected_song()
 
     # Start championship song on entry
-    def _handle_playing_championship_song(self: "Superstructure"):
+    def _handle_playing_championship_song(self: "Superstructure"): # type: ignore
         """
         !! ONLY WHEN CHAMPIONSHIP IS ENABLED !!
 
